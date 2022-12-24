c44bf9d3e4f93b60f34240445006a7db5c713f7e
duck420i69 <88267139+duck420i69@users.noreply.github.com>
2022-12-25 01:22:55 +0700

    Delete Renderer.h

--- Renderer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Shape.h"
-#include "Sampling.h"
-
-bool find_hit(const Scene& objects, const Ray& ray, Packet& result);
-
-vec4 trace(const Scene& objects, const Ray& ray, const int& depth, bool debug);
